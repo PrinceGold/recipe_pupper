cbf542b747d46f93cbf88ac7a53b9c7b818b64db
Richard Ellis <richevanellis@gmail.com>
2017-08-21 10:34:22 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+fetch(
+  "http://www.recipepuppy.com/api/?q=omelet"
+)
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log(response.status);
+      return;
+    }
+    response.json().then(function(data) {
+      console.log("test", response.url);
+      console.log(data)
+      displayData(data)
+    });
+  })
+  .catch(function(err) {
+    console.log("Fetch Error :-S", err);
+  });
+
+function displayData(data){
+	let htmlStr = ""
+	data.results.map(function(item){
+		htmlStr += `
+			<div id="xcom">
+			<img src='${item.thumbnail}'/>
+			<p> ${item.title} </p>
+			</div>
+
+		`
+
+		document.getElementById("whatever").innerHTML += htmlStr;
+	})
+
+	
+}
